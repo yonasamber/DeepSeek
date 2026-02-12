fec6092699e3607d998f690b20801b12ccffa97f
yonasamber <yonasamber@gmail.com>
2026-02-12 14:09:28 +0300

    headers updated in route js

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -9,6 +9,7 @@ export async function POST(req) {
   const headerPayload = await headers();
   const svixHeaders = {
     "svix-id": headerPayload.get("svix-id"),
+    "svix-timestamp": headerPayload.get("svix-timestamp"),
     "svix-signature": headerPayload.get("svix-signature"),
   };
   //get teh payload and verify
